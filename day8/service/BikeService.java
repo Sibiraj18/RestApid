dc2c3050e1ac6317018906431d4e114a629350f6
Sibiraj18 <108716705+Sibiraj18@users.noreply.github.com>
2023-09-01 15:53:13 +0530

    Add files via upload

---
+++ day8/service/BikeService.java
@@ -0,0 +1,28 @@
+package com.sibiraj.rest.day8.service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sibiraj.rest.day8.model.BikeModel;
+import com.sibiraj.rest.day8.repository.BikeRepo;
+
+
+
+@Service
+public class BikeService {
+	@Autowired
+	BikeRepo br;
+	public BikeModel add(BikeModel s) {
+		return br.save(s);
+	}
+	public List<BikeModel> getByYear(int year){
+		return br.getByYear(year);
+	}
+	public List<BikeModel> getByYearAndName(int year,String name){
+		return br.getByYearAndName(year, name);
+	}
+	public List<BikeModel> getByName(String name){
+		return br.getByName(name);
+	}
+}
